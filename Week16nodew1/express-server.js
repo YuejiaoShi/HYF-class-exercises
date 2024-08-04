e99ebe42998b75c2fddd4c8ebdcceeb08ff2332c
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-04 16:03:45 +0200

    another counting method

--- Week16nodew1/express-server.js
+++ Week16nodew1/express-server.js
@@ -7,15 +7,31 @@ app.get("/", (req, res) => {
 });
 
 app.get("/add/:num1/:num2", (req, res) => {
-    const num1 = parseFloat(req.params.num1);
-    const num2 = parseFloat(req.params.num2);
+  const num1 = parseFloat(req.params.num1);
+  const num2 = parseFloat(req.params.num2);
 
-    if (isNaN(num1) || isNaN(num2)) {
-      return res.status(400).send('Invalid numbers');
-    }
-    res.send(`The sum is ${num1 + num2}`);
+  if (isNaN(num1) || isNaN(num2)) {
+    return res.status(400).send("Invalid numbers");
+  }
+  res.send(`The sum is ${num1 + num2}`);
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+});
+
+app.get("/:number", (req, res) => {
+  res.writeHead(200, Headers);
+  let i = 0;
+  const number = parseInt(req.params.number);
+  const intervalId = setInterval(() => {
+    res.write(i.toString() + "\n"); // Write current value of i to the response
+    i++;
+
+    // Stop the interval when i exceeds the requested number
+    if (i > number) {
+      clearInterval(intervalId);
+      res.end(); // End the response
+    }
+  }, 1000);
 });
