18f890beb5b73084641331fff728584c490f5ba5
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-04 13:21:47 +0200

    import a sdd func from app.js

--- Week16nodew1/index.js
+++ Week16nodew1/index.js
@@ -1,5 +1,9 @@
 // import "./other.js";
 // import("./other.js");
-import { log1 } from "./other.js";
-log1();
+// import { log1 } from "./other.js";
+// log1();
+
+import { add } from "./add.js";
+
 console.log("hhhh");
+add(2, 5);
