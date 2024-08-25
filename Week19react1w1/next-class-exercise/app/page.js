367df9ba23761d5b09b1100f7181d317d0279650
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-08-25 13:47:07 +0200

    finish classExercise 1-5

--- Week19react1w1/next-class-exercise/app/page.js
+++ Week19react1w1/next-class-exercise/app/page.js
@@ -6,15 +6,64 @@ function Header() {
 }
 
 function Intro() {
-  return <div>Introduction .... </div>;
+  return <div>Introduction ....</div>;
+}
+
+function Greeting() {
+  return <h1>Hello, React!</h1>;
+}
+function Card() {
+  return (
+    <div>
+      <h2>Card Title</h2>
+      <p>This is a card component</p>
+    </div>
+  );
+}
+function Cards() {
+  return (
+    <>
+      <Card />
+      <Card />
+    </>
+  );
+}
+
+function Person() {
+  const name = "Yuejiao";
+  const age = 25;
+  if (age < 18) return <p>{name} is a minor.</p>;
+  return <p>{name} is an adult.</p>;
+}
+
+function Button() {
+  return <button className="btn btn-primary">Button</button>;
+}
+
+function DangerButton() {
+  return <button className="btn btn btn-primary">Dangerous</button>;
+}
+
+function TextInput() {
+  return <input placeholder="Type something here"></input>;
+}
+
+function ProfileImage() {
+  return (
+    <img src="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+  );
 }
 
 export default function Home() {
   return (
-    <>
-      <Header />
-      <Intro />
-      <main className={styles.main}>Hello!!!!</main>;
-    </>
+    <div className="">
+      <Greeting />
+      <Cards />
+      <Person />
+      <Button />
+      <DangerButton />
+      <TextInput />
+      <ProfileImage />
+    </div>
   );
 }
