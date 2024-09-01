7765ec8856dcd6353a1290f8ee85be9bc92e1b10
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-01 15:08:59 +0200

    change styles

--- Week19-21react1/next-class-exercise/components/Card.jsx
+++ Week19-21react1/next-class-exercise/components/Card.jsx
@@ -1,6 +1,6 @@
 function Card({ title, description, imageUrl }) {
   return (
-    <div className="flex flex-col items-center border-2 border-orange-300 w-96 m-3">
+    <div className="flex flex-col items-center border-2 border-orange-300 w-96 mt-3 mb-3">
       <h2 className="text-2xl">{title}</h2>
       <p className="text-orange-600">{description}</p>
       <img src={imageUrl} alt={`image of ${title}`} />
--- Week19-21react1/next-class-exercise/components/Layout.jsx
+++ Week19-21react1/next-class-exercise/components/Layout.jsx
@@ -1,6 +1,6 @@
 function Layout({ children }) {
   return (
-    <div>
+    <div className="m-3 flex flex-col items-center">
       <header className="text-4xl">HEADER</header>
       <main>{children}</main>
       <footer className="text-4xl">FOOTER</footer>
