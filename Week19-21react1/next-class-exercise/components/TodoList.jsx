876ddb9d5d9901a36605fb24d1e65a414944db74
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-01 15:32:10 +0200

    try to handle add new todo and delete todo from the end

--- Week19-21react1/next-class-exercise/components/TodoList.jsx
+++ Week19-21react1/next-class-exercise/components/TodoList.jsx
@@ -1,14 +1,36 @@
 import { useState } from "react";
+("ignore todos");
 
 function TodoList() {
-  const [todos, setTodos] = useState(["item1", "item2"]);
+  const [todos, setTodos] = useState(["todo1", "todo2"]);
+
+  function handleAdd(e) {
+    e.preventDefault();
+    setTodos([...todos, e.target.value]);
+  }
+  function handleDelete() {
+    setTodos(todos.slice(0, -1));
+  }
 
   return (
-    <ul>
-      {todos.map((todo, index) => (
-        <li key={index}>{todo}</li>
-      ))}
-    </ul>
+    <>
+      <form>
+        <input
+          type="text"
+          className="border-2"
+          placeholder="Add your new Todo..."
+        />
+        <button onSubmit={handleAdd}>Add todo</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index} className="list-disc">
+            {todo}
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleDelete}>Delete todo</button>
+    </>
   );
 }
 
