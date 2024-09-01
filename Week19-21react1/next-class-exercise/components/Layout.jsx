9f0e12da32c813373004c3b02c4cb5f18b89ece7
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-01 15:10:27 +0200

    change div to <section>

--- Week19-21react1/next-class-exercise/components/Layout.jsx
+++ Week19-21react1/next-class-exercise/components/Layout.jsx
@@ -1,10 +1,10 @@
 function Layout({ children }) {
   return (
-    <div className="m-3 flex flex-col items-center">
+    <section className="m-3 flex flex-col items-center">
       <header className="text-4xl">HEADER</header>
       <main>{children}</main>
       <footer className="text-4xl">FOOTER</footer>
-    </div>
+    </section>
   );
 }
 
