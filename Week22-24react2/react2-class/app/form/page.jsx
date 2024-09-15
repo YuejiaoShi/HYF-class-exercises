34004539741c57b1e429479b4c684f951da20483
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-15 15:18:58 +0200

    ref have to initialize with null instead of ''

--- Week22-24react2/react2-class/app/form/page.jsx
+++ Week22-24react2/react2-class/app/form/page.jsx
@@ -1,24 +1,38 @@
+"use client";
+
 import { useRef } from "react";
 
 function FormPage() {
   const formRef = useRef({
-    title: "",
-    content: "",
-    author: "",
-    date: "",
+    title: null,
+    content: null,
+    author: null,
+    date: null,
   });
 
   return (
     <form>
       <h1>Take a Note</h1>
-      <label>Note Title: </label>
+      <label htmlFor="title">Note Title: </label>
       <input type="text" name="title" id="title" ref={formRef.current.title} />
       <br className="formbr" />
-      <label>Note Content: </label>
-      <textarea type="text" name="content" id="content" rows={10} cols={50} ref={formRef.current.content} />
+      <label htmlFor="content">Note Content: </label>
+      <textarea
+        type="text"
+        name="content"
+        id="content"
+        rows={10}
+        cols={50}
+        ref={formRef.current.content}
+      />
       <br className="formbr" />
-      <label>Note Author: </label>
-      <input type="text" name="author" id="author" ref={formRef.current.author} />
+      <label htmlFor="author">Note Author: </label>
+      <input
+        type="text"
+        name="author"
+        id="author"
+        ref={formRef.current.author}
+      />
       <br className="formbr" />
       <label htmlFor="date">Note Date: </label>
       <input type="date" name="date" id="date" ref={formRef.current.date} />
