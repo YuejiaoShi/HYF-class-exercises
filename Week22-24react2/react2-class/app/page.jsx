190bed94fc5108728dc9902d590a0017c9c5c8e0
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-15 15:12:17 +0200

    ignore the styles

--- Week22-24react2/react2-class/app/page.jsx
+++ Week22-24react2/react2-class/app/page.jsx
@@ -1,13 +1,12 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import styles from "./page.module.css";
 
 export default function Home() {
   const router = useRouter();
 
   return (
-    <div className={styles.page}>
+    <div>
       <h1>Welcome to my page</h1>
 
       <button onClick={() => router.push("/notes")}>Go to notes</button>
