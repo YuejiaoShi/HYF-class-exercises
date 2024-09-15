252837affdae308f9b1e827a801ff0396f33b213
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-15 13:17:18 +0200

    create a form state to handle the submit

--- Week22-24react2/react2-class/app/notes/page.jsx
+++ Week22-24react2/react2-class/app/notes/page.jsx
@@ -1,12 +1,33 @@
+import { useState } from "react";
+
 function NotesPage() {
+  const [form, setForm] = useState({ noteTaker: "", noteId: "" });
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
+
   return (
     <main>
       <h1>Notes Page</h1>
       <label htmlFor="note-taker">Note Taker:</label>
-      <input id="note-taker" name="notes-taker" type="text"></input>
+      <input
+        name="noteTaker"
+        id="notes-taker"
+        type="text"
+        onChange={handleChange}
+      ></input>
 
       <label htmlFor="note-id">Note ID:</label>
-      <input id="note-id" name="note-id" type="text"></input>
+      <input
+        name="noteId"
+        id="note-id"
+        type="text"
+        onChange={handleChange}
+      ></input>
+
+      <button type="submit">Go to note</button>
     </main>
   );
 }
