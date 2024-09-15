573e5f2ae30cac3037e98bea4f74d330f9e0d930
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-15 13:07:19 +0200

    convert all query into an object, map over query which share the same query key, but the value will only accept the last one for this query

--- Week22-24react2/react2-class/app/notes/[noteId]/page.jsx
+++ Week22-24react2/react2-class/app/notes/[noteId]/page.jsx
@@ -5,13 +5,19 @@ import { usePathname, useSearchParams } from "next/navigation";
 function Note({ params }) {
   const path = usePathname();
   const query = useSearchParams();
-
+  const allQuery = Object.fromEntries(query.entries());
 
   return (
     <main>
       <p>{path}</p>
       <p>{JSON.stringify(params)}</p>
       <p>{query.get("notemaker")}</p>
+      <div>
+        {query.getAll("notemaker").map((notemaker) => (
+          <p>{notemaker}</p>
+        ))}
+      </div>
+      <p>{JSON.stringify(allQuery)}</p>
     </main>
   );
 }
