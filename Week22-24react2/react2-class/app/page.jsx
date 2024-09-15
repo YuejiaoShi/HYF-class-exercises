7983861ee671832370ef48b8ea8a091b031887b2
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-15 12:46:08 +0200

    call useRouter to redirect to note page

--- Week22-24react2/react2-class/app/page.jsx
+++ Week22-24react2/react2-class/app/page.jsx
@@ -1,9 +1,16 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <div className={styles.page}>
       <h1>Welcome to my page</h1>
+
+      <button onClick={() => router.push("/notes")}>Go to notes</button>
     </div>
   );
 }
