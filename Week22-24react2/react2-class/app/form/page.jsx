a2b53b4844de3237279ad9723fda46e65f6bd727
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-15 15:16:46 +0200

    create Ref and assign each in ref object to the inputs

--- Week22-24react2/react2-class/app/form/page.jsx
+++ Week22-24react2/react2-class/app/form/page.jsx
@@ -1,18 +1,27 @@
+import { useRef } from "react";
+
 function FormPage() {
+  const formRef = useRef({
+    title: "",
+    content: "",
+    author: "",
+    date: "",
+  });
+
   return (
     <form>
       <h1>Take a Note</h1>
       <label>Note Title: </label>
-      <input type="text" name="title" id="title" />
+      <input type="text" name="title" id="title" ref={formRef.current.title} />
       <br className="formbr" />
       <label>Note Content: </label>
-      <textarea type="text" name="content" id="content" rows={10} cols={50} />
+      <textarea type="text" name="content" id="content" rows={10} cols={50} ref={formRef.current.content} />
       <br className="formbr" />
       <label>Note Author: </label>
-      <input type="text" name="author" id="author" />
+      <input type="text" name="author" id="author" ref={formRef.current.author} />
       <br className="formbr" />
       <label htmlFor="date">Note Date: </label>
-      <input type="date" name="date" id="date" />
+      <input type="date" name="date" id="date" ref={formRef.current.date} />
       <br className="formbr" />
       <button type="submit">Submit Note</button>
     </form>
