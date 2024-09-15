2a25cea81482681b0ca893a59548814623aa4707
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-15 15:23:29 +0200

    separate each ref

--- Week22-24react2/react2-class/app/form/page.jsx
+++ Week22-24react2/react2-class/app/form/page.jsx
@@ -3,39 +3,38 @@
 import { useRef } from "react";
 
 function FormPage() {
-  const formRef = useRef({
-    title: null,
-    content: null,
-    author: null,
-    date: null,
-  });
+  const titleRef = useRef(null);
+  const contentRef = useRef(null);
+  const authorRef = useRef(null);
+  const dateRef = useRef(null);
+
+  const clickTitle = () => {
+    if (titleRef.current) {
+      titleRef.current.focus();
+    }
+  };
 
   return (
     <form>
       <h1>Take a Note</h1>
       <label htmlFor="title">Note Title: </label>
-      <input type="text" name="title" id="title" ref={formRef.current.title} />
+      <input ref={titleRef} type="text" name="title" id="title" />
       <br className="formbr" />
       <label htmlFor="content">Note Content: </label>
       <textarea
+        ref={contentRef}
         type="text"
         name="content"
         id="content"
         rows={10}
         cols={50}
-        ref={formRef.current.content}
       />
       <br className="formbr" />
       <label htmlFor="author">Note Author: </label>
-      <input
-        type="text"
-        name="author"
-        id="author"
-        ref={formRef.current.author}
-      />
+      <input ref={authorRef} x type="text" name="author" id="author" />
       <br className="formbr" />
       <label htmlFor="date">Note Date: </label>
-      <input type="date" name="date" id="date" ref={formRef.current.date} />
+      <input ref={dateRef} type="date" name="date" id="date" />
       <br className="formbr" />
       <button type="submit">Submit Note</button>
     </form>
