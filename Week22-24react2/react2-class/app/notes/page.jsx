e9afb7daa5e0bfbe0f3fa957cad9d60e4d7fd6e3
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-15 12:40:18 +0200

    add two inputs

--- Week22-24react2/react2-class/app/notes/page.jsx
+++ Week22-24react2/react2-class/app/notes/page.jsx
@@ -2,7 +2,11 @@ function NotesPage() {
   return (
     <main>
       <h1>Notes Page</h1>
-      
+      <label htmlFor="note-taker">Note Taker:</label>
+      <input id="note-taker" name="notes-taker" type="text"></input>
+
+      <label htmlFor="note-id">Note ID:</label>
+      <input id="note-id" name="note-id" type="text"></input>
     </main>
   );
 }
