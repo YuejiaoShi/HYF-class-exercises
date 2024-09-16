7b32fddd32e268f8dffa4208309d0db5fe81bf6c
YuejiaoShi <shiyuejiaosyj@gmail.com>
2024-09-16 21:59:01 +0200

    Use the NASA API to fetch EPIC images

--- Week22-24react2/react2-class/app/blogs/page.jsx
+++ Week22-24react2/react2-class/app/blogs/page.jsx
@@ -1,9 +1,38 @@
 "use client";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 function BlogPage() {
+  const [EPICImages, setEPICImages] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchEPICImages = async () => {
+      try {
+        const EPICImages = await fetch(
+          "https://api.nasa.gov/EPIC/api/natural?api_key=DEMO_KEY"
+        );
+        if (!EPICImages.ok) throw new Error("Error fetching rover photos");
+        const EPICImagesData = await EPICImages.json();
+        setEPICImages(EPICImagesData);
+      } catch (err) {
+        setError(err.message);
+      }
+    };
+
+    fetchEPICImages();
+  }, []);
   const pathname = usePathname();
-  return <p>Current pathname: {pathname}</p>;
+  return (
+    <>
+      <p>Current pathname: {pathname}</p>;
+      <p>
+        {EPICImages.map((image) => (
+          <img src={image.url} alt={image.identifier} />
+        ))}
+      </p>
+    </>
+  );
 }
 
 export default BlogPage;
